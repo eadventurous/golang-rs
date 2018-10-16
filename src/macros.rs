441687240e586239fbdebe65f1dcc30a51986d04
ivan tkachenko <ratijas@users.noreply.github.com>
2018-10-16 23:29:18 +0300

    awesome macro_rules tree!

--- src/macros.rs
+++ src/macros.rs
@@ -49,8 +49,100 @@ macro_rules! constant {
     };
 }
 
+/// Tree literals with [`id_tree`] crate and `tree!` macro with ease!
+///
+/// # Examples
+///
+/// Empty tree:
+/// ```
+/// tree!();
+/// ```
+///
+/// Tree of one element:
+/// ```
+/// tree!("root");
+/// ```
+///
+/// Same as above, no children:
+/// ```
+/// tree!("root" => {});
+/// ```
+///
+/// Root element has only one children:
+/// ```
+/// tree!("root" => {"child"});
+/// ```
+///
+/// Root element has multiple children:
+/// ```
+/// tree!("root" => {"one", "four", "two"});
+/// ```
+///
+/// Tree of height 4:
+/// ```
+/// tree!("parent" => {"daughter", "son" => {"grandson", "granddaughter"}});
+/// ```
+///
+/// General pattern:
+/// ```bnf
+/// <Nodes> ::= ( <Node> "," )*
+/// <Node> ::= <Value> <Children>
+/// <Children> ::= "=>" "{" <Nodes> "}" | ""
+/// ```
+///
+/// [`id_tree`]: https://crates.io/crates/id_tree
+macro_rules! tree {
+    () => (
+        ::id_tree::TreeBuilder::new().build()
+    );
+    ($root:expr) => {{
+        use id_tree::*;
+
+        ::id_tree::TreeBuilder::new().with_root(Node::new($root)).build()
+    }};
+    ($root:expr => { $($children:tt)* }) => {{
+        let mut tree = tree!($root);
+        {
+            let root_node = tree.root_node_id().unwrap().clone();
+            tree!(@ tree, root_node, $($children)*);
+        }
+        tree
+    }};
+
+    // # Private implementation
+
+    // empty base case
+    // `let _ =` used to suppress 'unused' warning.
+    (@ $tree:expr, $parent:expr) => {{ let _ = $parent; }};
+    (@ $tree:expr, $parent:expr,) => {{ let _ = $parent; }};
+
+    // one-child base case
+    (@ $tree:expr, $parent:expr, $child:expr) => (
+        $tree.insert(Node::new($child), ::id_tree::InsertBehavior::UnderNode(&$parent)).unwrap()
+    );
+
+    // parent=>children base case
+    (@ $tree:expr, $parent:expr, $child:expr => { $($grandchildren:tt)* }) => {{
+        let new_parent = tree!(@ $tree, $parent, $child);
+        tree!(@ $tree, new_parent, $($grandchildren)*);
+    }};
+
+    // comma-separated recursive case
+    (@ $tree:expr, $parent:expr, $child:expr, $($rest:tt)*) => {{
+        tree!(@ $tree, $parent, $child);
+        tree!(@ $tree, $parent, $($rest)*);
+    }};
+
+    // parent=>children recursive case
+    (@ $tree:expr, $parent:expr, $child:expr => { $($grandchildren:tt)* }, $($rest:tt)*) => {
+        tree!(@ $tree, $parent, $child => { $($grandchildren)* });
+        tree!(@ $tree, $parent, $($rest)*);
+    };
+}
+
 #[cfg(test)]
 mod test {
+    use id_tree::*;
     use std::collections::HashMap;
     use std::collections::HashSet;
 
@@ -108,5 +200,73 @@ mod test {
 
         assert_eq!("42", xs(1, 2, 3, 4));
         assert_eq!("42", xs(5, 6, 7, 8));
+    }
+
+    #[test]
+    fn test_tree_empty() {
+        let _tree: Tree<i32> = tree!();
+    }
+
+    #[test]
+    fn test_tree_single_root() {
+        let _tree: Tree<i32> = tree!(42);
+    }
+
+    #[test]
+    fn test_tree_without_children() {
+        let _tree: Tree<i32> = tree!(42 => {});
+    }
+
+    #[test]
+    fn test_tree_with_children() {
+        let tree: Tree<i32> = tree!(42 => {37, 99});
+
+        let mut nodes = tree
+            .traverse_pre_order(tree.root_node_id().unwrap())
+            .unwrap();
+        assert_eq!(&42, nodes.next().unwrap().data());
+        assert_eq!(&37, nodes.next().unwrap().data());
+        assert_eq!(&99, nodes.next().unwrap().data());
+        assert!(nodes.next().is_none());
+    }
+
+    #[test]
+    fn test_tree_with_children_nested() {
+        let tree: Tree<i32> = tree!(42 => {37, 99 => {25}, 80 => {9001, 1337}, 0 => {}});
+
+        let pre_order = vec![42, 37, 99, 25, 80, 9001, 1337, 0];
+        let nodes = tree
+            .traverse_pre_order(tree.root_node_id().unwrap())
+            .unwrap()
+            .map(Node::data)
+            .map(Clone::clone)
+            .collect::<Vec<_>>();
+        assert_eq!(pre_order, nodes);
+    }
+
+    #[test]
+    #[cfg(skip)]
+    fn test_tt_macro() {
+        macro_rules! tt {
+            () => {println!("tt(empty)")};
+            ($node:expr) => {println!("tt(node).")};
+            ($node:expr, $($rest:tt)*) => {
+                print!("tt(node) and... ");
+                tt!($($rest)*);
+            };
+            ($key:expr => $value:expr) => {
+                println!("tt(key({}) => value({})).", $key, $value);
+            };
+            ($key:expr => $value:expr, $($rest:tt)*) => {
+                print!("tt(key({}) => value({})) and... ", $key, $value);
+                tt!($($rest)*);
+            }
+        }
+
+        tt!();
+        tt!(42);
+        tt!('a' => 'b');
+        tt!('a' => 'b');
+        tt!('a' => 'b', 'c', 'd' => 'e');
     }
 }
