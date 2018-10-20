cd2b44382264df53488c35513949dce98fb295c5
ivan tkachenko <ratijas@users.noreply.github.com>
2018-10-20 05:09:39 +0300

    Golang tokens descriptors

--- src/syn/golang/mod.rs
+++ src/syn/golang/mod.rs
@@ -1,12 +1,17 @@
+use id_tree::*;
+use lang::golang::make_lexer;
 use syn::bnf::Grammar;
 use syn::ebnf::{self, Syntax};
 use syn::predictive_parser::parse_tokens;
-use id_tree::*;
-use lang::golang::make_lexer;
 
 pub fn ebnf() -> Syntax {
     let source = include_str!("golang.bnf");
-    let syntax = ebnf::Parser::new(source, "golang.bnf").parse().unwrap();
+    let syntax = ebnf::Parser::new(source, "golang.bnf")
+        .parse()
+        .unwrap_or_else(|e| {
+            println!("{}", e);
+            panic!();
+        });
     println!("{}", syntax);
     syntax
 }
